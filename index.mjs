e323cb5cd7bf11f3574b00cde092c8d9d0f0bb65
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-06-26 17:03:59 +0700

    update multer

--- index.mjs
+++ index.mjs
@@ -26,6 +26,7 @@ dotenv.config();
 
 const PORT = process.env.SERVER_PORT || 5000;
 const app = express();
+const upload = multer();
 
 app.use(cors({
   origin: 'http://localhost:3000', 
@@ -63,6 +64,7 @@ app.use(cors({
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.text());
+app.use(upload.array());
 
 // API Routes
 app.use('/api/', userRoutes);
