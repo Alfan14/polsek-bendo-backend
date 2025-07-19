2e54ddaf19bb063234baae0b6e54b71587e7fe68
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-07-19 19:14:55 +0700

    update cors

--- index.mjs
+++ index.mjs
@@ -29,7 +29,7 @@ const PORT = process.env.SERVER_PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: process.env.PROD_ORIGIN, 
+  origin: [process.env.PROD_ORIGIN,process.env.ORIGIN], 
   credentials: true                
 }));
 
