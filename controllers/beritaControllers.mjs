2785dd74332d5d66e62059d6e6397a7fb70b08a2
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-08-18 16:37:54 +0700

    upadte newsControllers

--- controllers/beritaControllers.mjs
+++ controllers/beritaControllers.mjs
@@ -21,11 +21,11 @@ const getNewsById = (request, response) => {
 }
 
 const createNews = (request, response) => {
-  const { title, slug, excerpt, content, author_id, category_id, url_gambar_unggulan, status, published_at , created_at, updated_at} = request.body
+  const { title, slug, excerpt, content, author_id, category, url_gambar_unggulan, status, published_at , created_at, updated_at} = request.body
 
   pool.query(
-    'INSERT INTO news (title, slug, excerpt, content, author_id, category_id, url_gambar_unggulan, status, published_at, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
-    [title, slug, excerpt, content, author_id, category_id, url_gambar_unggulan, status, published_at, created_at, updated_at],
+    'INSERT INTO news (title, slug, excerpt, content, author_id, category, url_gambar_unggulan, status, published_at, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
+    [title, slug, excerpt, content, author_id, category, url_gambar_unggulan, status, published_at, created_at, updated_at],
     (error, results) => {
       if (error) {
         throw error;
@@ -36,11 +36,11 @@ const createNews = (request, response) => {
 
 const updateNews = (request, response) => {
   const id = parseInt(request.params.id)
-  const { title, slug, excerpt, content, author_id, category_id, url_gambar_unggulan, status, published_at, created_at, updated_at } = request.body
+  const { title, slug, excerpt, content, author_id, category, url_gambar_unggulan, status, published_at, created_at, updated_at } = request.body
 
   pool.query(
-    'UPDATE news SET title = $1, slug = $2, excerpt = $3, content = $4, author_id = $5, category_id = $6, url_gambar_unggulan = $7, status = $8, published_at = $9, created_at = $10, updated_at = $11, published_at = $12 WHERE id = $13',
-    [title, slug, excerpt, content, author_id, category_id, url_gambar_unggulan, ,status, published_at , created_at, updated_at, id],
+    'UPDATE news SET title = $1, slug = $2, excerpt = $3, content = $4, author_id = $5, category = $6, url_gambar_unggulan = $7, status = $8, published_at = $9, created_at = $10, updated_at = $11, published_at = $12 WHERE id = $13',
+    [title, slug, excerpt, content, author_id, category, url_gambar_unggulan, ,status, published_at , created_at, updated_at, id],
     (error, results) => {
       if (error) {
         throw error
@@ -52,7 +52,7 @@ const updateNews = (request, response) => {
 
 const patchNews = async (request, response) => {
     const id = parseInt(request.params.id);
-    const { title, slug, excerpt, content, author_id, category_id, url_gambar_unggulan, status, published_at, created_at, updated_at } = request.body;
+    const { title, slug, excerpt, content, author_id, category, url_gambar_unggulan, status, published_at, created_at, updated_at } = request.body;
 
     const fields = [];
     const values = [];
@@ -78,9 +78,9 @@ const patchNews = async (request, response) => {
       fields.push(`author_id = $${valueIndex++}`);
       values.push(author_id);
     }
-    if (category_id) {
-      fields.push(`category_id = $${valueIndex++}`);
-      values.push(category_id);
+    if (category) {
+      fields.push(`category = $${valueIndex++}`);
+      values.push(category);
     }
     if (url_gambar_unggulan) {
       fields.push(`url_gambar_unggulan = $${valueIndex++}`);
